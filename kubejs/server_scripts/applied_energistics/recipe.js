47b7232f3e208dbab9ad575450b61bd700b73df0
sponeru <o0sponge0o_minecraft_flat@yahoo.co.jp>
2025-06-03 20:30:47 +0900

    0.4.0

--- kubejs/server_scripts/applied_energistics/recipe.js
+++ kubejs/server_scripts/applied_energistics/recipe.js
@@ -52,6 +52,17 @@ ServerEvents.recipes(event => {
         D: 'minecraft:diamond'
     })
 
+    event.shaped(Item.of('expatternprovider:infinity_cell', '{record:{"#c":"ae2:i",id:"xtonesreworked:xtone_tile"}}'), [
+        'GBG',
+        'BCB',
+        'DDD'
+    ], {
+        G: 'ae2:quartz_glass',
+        B: 'xtonesreworked:xtone_tile',
+        C: 'ae2:cell_component_16k',
+        D: 'minecraft:diamond'
+    })
+
     event.shaped(Item.of("mek1000:charged_redstone_block"), [
         'CCC',
         'CCC',
